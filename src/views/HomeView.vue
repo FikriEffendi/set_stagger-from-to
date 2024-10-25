8488a1f8377cace285acf84349110c125400d403
aerochops <150325960+aerochops@users.noreply.github.com>
2024-10-25 17:18:20 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="grid h-screen grid-cols-3 place-items-center">
+    <div class="relative">
+      <div class="aspect-[9/16] w-72">
+        <img
+          src="../../public/img1.jpg"
+          class="object-cover w-full h-full"
+          alt=""
+        />
+      </div>
+      <div
+        class="absolute inset-0 flex items-center justify-center text-2xl font-bold text-white"
+      >
+        <div>Halo</div>
+      </div>
+    </div>
+    <div>
+      <div class="aspect-[9/16] w-72">
+        <img
+          src="../../public/img2.jpg"
+          class="object-cover w-full h-full"
+          alt=""
+        />
+      </div>
+    </div>
+    <div>
+      <div class="aspect-[9/16] w-72">
+        <img
+          src="../../public/img3.jpg"
+          class="object-cover w-full h-full"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
